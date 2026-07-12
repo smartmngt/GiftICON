5c307c9df802f5271720d4e26ffe1bf90ae3613a
smartmngt <smartmngt@gmail.com>
2026-07-12 20:02:28 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'gifticon-v1';
+const ASSETS = [
+  '/GiftICON/',
+  '/GiftICON/index.html',
+  '/GiftICON/manifest.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
