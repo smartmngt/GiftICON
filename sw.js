1224a2b1296eab711836d43c199cbf2bf95efb7b
smartmngt <smartmngt@gmail.com>
2026-07-13 00:12:08 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,3 @@
-const CACHE = 'gifticon-v8';
-const ASSETS = [
-  '/GiftICON/',
-  '/GiftICON/index.html',
-  '/GiftICON/manifest.json',
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-
 self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request));
 });
